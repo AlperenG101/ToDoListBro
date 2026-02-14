16302823b322be2edfba5ee9af284894fa2bb1ba
AlperenG101 <alperen2009g@gmail.com>
2026-02-14 10:09:18 +0300

    Initial commit

---
+++ ToDoListBro/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace ToDoListBro
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gorev_txt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ekle_button = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gorev_txt
+            // 
+            this.gorev_txt.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gorev_txt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.gorev_txt.Location = new System.Drawing.Point(80, 13);
+            this.gorev_txt.Name = "gorev_txt";
+            this.gorev_txt.Size = new System.Drawing.Size(611, 26);
+            this.gorev_txt.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Görev ekle:";
+            // 
+            // ekle_button
+            // 
+            this.ekle_button.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ekle_button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(106)))), ((int)(((byte)(153)))), ((int)(((byte)(251)))));
+            this.ekle_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ekle_button.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.ekle_button.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(34)))), ((int)(((byte)(99)))));
+            this.ekle_button.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(156)))), ((int)(((byte)(187)))), ((int)(((byte)(252)))));
+            this.ekle_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ekle_button.ForeColor = System.Drawing.Color.White;
+            this.ekle_button.Location = new System.Drawing.Point(697, 13);
+            this.ekle_button.Name = "ekle_button";
+            this.ekle_button.Size = new System.Drawing.Size(75, 27);
+            this.ekle_button.TabIndex = 2;
+            this.ekle_button.Text = "Ekle";
+            this.ekle_button.UseVisualStyleBackColor = false;
+            this.ekle_button.Click += new System.EventHandler(this.ekle_button_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 16);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(753, 480);
+            this.panel1.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.ForeColor = System.Drawing.Color.White;
+            this.groupBox1.Location = new System.Drawing.Point(13, 50);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(759, 499);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Görevler";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(34)))), ((int)(((byte)(99)))));
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.ekle_button);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gorev_txt);
+            this.Controls.Add(this.groupBox1);
+            this.MinimumSize = new System.Drawing.Size(640, 480);
+            this.Name = "Form1";
+            this.Text = "ToDoListBro";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox gorev_txt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button ekle_button;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ ToDoListBro/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToDoListBro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int arttirilanKonum = 10;
+        List<string> liste = new List<string>();
+        private void ekle_button_Click(object sender, EventArgs e)
+        {
+            CheckBox yeniNot = new CheckBox();
+            Button sil_button = new Button();
+            yeniNot.ForeColor = Color.White;
+            yeniNot.Text = gorev_txt.Text;
+            yeniNot.CheckedChanged += (s, ev) =>
+            {
+                if (yeniNot.Checked)
+                {
+                    yeniNot.Font = new Font(yeniNot.Font, FontStyle.Italic | FontStyle.Strikeout);
+                    yeniNot.ForeColor = Color.Gray;
+                }
+                else
+                {
+                    yeniNot.Font = new Font(yeniNot.Font, FontStyle.Regular);
+                    yeniNot.ForeColor = Color.White;
+                }
+            };
+
+            yeniNot.Location = new Point(10, arttirilanKonum);
+            yeniNot.AutoSize = true;
+            sil_button.Location = new Point(panel1.Right - 45, arttirilanKonum);
+            sil_button.Text = "Sil";
+            sil_button.BackColor = Color.White;
+            sil_button.ForeColor = Color.Black;
+            sil_button.Width = 40;
+            sil_button.Click += (s, ev) =>
+            {
+                liste.Remove(yeniNot.Text);
+                panel1.Controls.Remove(yeniNot);
+                panel1.Controls.Remove(sil_button);
+                int silinenYukseklik = 25; 
+                foreach (Control ctrl in panel1.Controls)
+                {
+                    if (ctrl.Top > yeniNot.Top)
+                    {
+                        ctrl.Top -= silinenYukseklik;
+                    }
+                }
+                arttirilanKonum -= silinenYukseklik;
+                panel1.Controls.Remove(yeniNot);
+                panel1.Controls.Remove(sil_button);
+                yeniNot.Dispose();
+                sil_button.Dispose();
+            };
+            panel1.Controls.Add(yeniNot);
+            panel1.Controls.Add(sil_button);
+            yeniNot.BringToFront();
+            liste.Add(yeniNot.Text);
+            gorev_txt.Text = "";
+            arttirilanKonum += 25;
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            panel1.BringToFront();
+            if (File.Exists(Path.Combine(Application.StartupPath, "index.txt")))
+            {
+                string[] okunanlar = File.ReadAllLines(Path.Combine(Application.StartupPath, "index.txt"));
+                foreach (string gorev in okunanlar)
+                {
+                    if (string.IsNullOrWhiteSpace(gorev)) continue;
+                    CheckBox yeniNot = new CheckBox();
+                    Button sil_button = new Button();
+                    sil_button.Location = new Point(panel1.Right -45, arttirilanKonum);
+                    sil_button.Text = "Sil";
+                    sil_button.BackColor = Color.White;
+                    sil_button.ForeColor = Color.Black;
+                    sil_button.Width = 40;
+                    yeniNot.Text = gorev;
+                    if (yeniNot.Checked == true)
+                    {
+                        yeniNot.Font = new Font(yeniNot.Font, FontStyle.Italic | FontStyle.Strikeout);
+                    }
+                    yeniNot.ForeColor = Color.White;
+                    yeniNot.AutoSize = true;
+                    yeniNot.Location = new Point(10, arttirilanKonum);
+                    liste.Add (gorev);
+                    sil_button.Click += (s, ev) =>
+                    {
+                        liste.Remove(yeniNot.Text);
+                        panel1.Controls.Remove(yeniNot);
+                        panel1.Controls.Add(sil_button);
+                        panel1.Controls.Remove(sil_button);
+                        int silinenYukseklik = 25; 
+                        foreach (Control ctrl in panel1.Controls)
+                        {
+                            if (ctrl.Top > yeniNot.Top)
+                            {
+                                ctrl.Top -= 25; 
+                            }
+                        }
+                        arttirilanKonum -= silinenYukseklik;
+                        yeniNot.Dispose();
+                        sil_button.Dispose();
+                    };
+
+                    panel1.Controls.Add (yeniNot);
+                    panel1.Controls.Add (sil_button);
+
+                    arttirilanKonum += 25;
+
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            File.WriteAllLines(Path.Combine(Application.StartupPath, "index.txt"), liste);
+        }
+    }
+}
